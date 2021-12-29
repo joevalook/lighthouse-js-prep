55b1446d138572742f2b85c9639dd01d0a64c957
Joe Valookaran <joevalookaran@hotmail.com>
2021-12-29 21:14:46 +0000

    tweaked it a little for an array

---
+++ hello.js
@@ -0,0 +1,9 @@
+const sayHello = function(name){
+    for ( let object of name) {
+      console.log("Hello, " +object)
+    }
+  }
+
+  j = ["ally", "osbald", "Thornton", "Gina", "Jordan"]
+
+sayHello(j);
