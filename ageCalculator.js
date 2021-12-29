5d981274210fccffd5abb7ca20b8ac3fac623257
Joe Valookaran <joevalookaran@hotmail.com>
2021-12-29 22:05:39 +0000

    added age calculator function

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(x,y,z){
+  let w =  (z - y)
+  return (x + " is " + w + " years old.");
+}
+
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
